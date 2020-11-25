684428c1ae3dabf35dc193656b84431aafa4c887
AlpenGod <dominob1999@gmail.com>
2020-11-25 13:48:02 +0100

    'fixed'

--- app/src/main/java/com/example/aftercovid/MainActivity.java
+++ app/src/main/java/com/example/aftercovid/MainActivity.java
@@ -14,31 +14,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-<<<<<<< HEAD
         Button buttonMenu = (Button)findViewById(R.id.buttonMenu);
         buttonMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openMainMenuActivity();
-=======
+            }});
+
         Button buttonTest = (Button)findViewById(R.id.buttonTest);
         buttonTest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openTestActivity();
->>>>>>> master
             }
 
         });
     }
 
-<<<<<<< HEAD
     private void openMainMenuActivity() {
         Intent intent = new Intent(this, MainMenuActivity.class);
-=======
+        startActivity(intent);
+    }
     public void openTestActivity(){
         Intent intent = new Intent(this, TestActivity.class);
->>>>>>> master
         startActivity(intent);
     }
 }
